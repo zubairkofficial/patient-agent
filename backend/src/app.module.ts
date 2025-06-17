65de3f0ed93164e8f56051992e8d44f2ed1a30ea
Cyberify Developer <63253113+zubairkofficial@users.noreply.github.com>
2025-06-17 13:11:29 +0500

    patient agent initialization'

---
+++ backend/src/app.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadModels: true,
+      models: [],
+      synchronize: process.env.DB_SYNCHRONIZE == 'true' ? true : false,
+      logging: true,
+
+      sync: {
+        force: process.env.NODE_ENV == 'development' ? false : false,
+        alter: process.env.NODE_ENV == 'development' ? true : false,
+      },
+      retryDelay: 3000,
+    }),
+
+
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
